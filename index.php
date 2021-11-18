31cc24248fd10556e4c4963bd8508d279bf59fe6
kevin_smrt <kevinsanmartin@live.fr>
2021-11-18 17:10:09 +0100

    react template

--- index.php
+++ index.php
@@ -1,20 +1,6 @@
-<!DOCTYPE html>
-<html lang="fr">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Index</title>
-</head>
-
-<body>
-    <div id="root"></div>
-
-    <script src="https://unpkg.com/react@17/umd/react.production.min.js" crossorigin></script>
-    <script src="https://unpkg.com/react-dom@17/umd/react-dom.production.min.js" crossorigin></script>
-
-    <script src="App.js"></script>
-</body>
-
-</html>
+<?php
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
+    $name = "Bernardo";
+    echo json_encode($name);
+?>
