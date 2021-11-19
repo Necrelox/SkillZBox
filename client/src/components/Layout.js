15117162417f4ee7573fabaa4204d7828e2cd0b6
kevin_smrt <kevinsanmartin@live.fr>
2021-11-19 13:38:09 +0100

    redefine color theme

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -3,6 +3,10 @@ import '../css/layout.css';
 export default function Layout(props) {
     return (
         <body>
+            <div class="bg"></div>
+            <div class="bg bg2"></div>
+            <div class="bg bg3"></div>
+            <div class="blur"></div>
             <div class="card">
                 {props.children}
             </div>
--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -1,5 +1,5 @@
 export default function Home() {
     return (
-        <h1>HomePage</h1>
+        <h1></h1>
     )
 }
