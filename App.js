d5b6bfb9fce5e455397fd4c85d012fca9f20e8f1
Ruby <nicolas.da-silva@epitech.eu>
2021-11-19 16:04:20 +0100

    remove old files

--- App.js
+++
@@ -1,9 +0,0 @@
-function App() {
-    return React.createElement(
-        'h1',
-        null,
-        'Salut'
-    );
-}
-
-ReactDOM.render(React.createElement(App, null), document.getElementById('root'));
--- src/App.js
+++
@@ -1,7 +0,0 @@
-function App() {
-    return (
-        <h1>Salut</h1>
-    )
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
