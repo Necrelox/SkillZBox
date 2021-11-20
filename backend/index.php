ab6d2633632a883f5618897e78a818cb4073bc1f
Necrelox <57537266+Necrelox@users.noreply.github.com>
2021-11-20 23:12:03 +0100

    Delete backend directory

--- backend/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require_once("Controller/MainController.php")
-    header("Access-Control-Allow-Origin: *");   
-    header("Content-Type: application/json; charset=UTF-8");
-
-
-//    echo json_encode($name);
-?>
