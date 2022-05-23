c8166cde73612b353487197805551c6c581c8226
Ruby <nicolas.da-silva@epitech.eu>
2022-05-23 19:37:04 +0200

    #39: add generic method for handle sqlMessageError
    For the moment, only duplicate entry is managed else the sqlMessage is return.

--- back/src/server/database/skillzboxDatabaseKnex.ts
+++ back/src/server/database/skillzboxDatabaseKnex.ts
@@ -20,7 +20,6 @@ export class SkillzboxDatabaseKnex {
                 user: process.env.DATABASE_USER,
                 password: process.env.DATABASE_PASSWORD,
                 database: process.env.DATABASE_NAME,
-
             },
             pool: {
                 min: 0,
@@ -28,6 +27,16 @@ export class SkillzboxDatabaseKnex {
             },
             acquireConnectionTimeout: 10000,
         })
+    }
+
+    public static createBetterSqlMessageError(sqlCode: string, sqlMessage: string) {
+        if (sqlCode === 'ER_DUP_ENTRY') {
+            const messageSplit = sqlMessage.split('\'');
+            const value = messageSplit[1];
+            const column = (messageSplit[3]?.split('.')[1])?.split('_')[0];
+            return `This ${column} : ${value} is already used.`;
+        }
+        return sqlMessage;
     }
 
     public static getInstance(): any {
